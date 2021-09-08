c0986a029f75f90d340d1cf9dfbb4115a8fe333f
idkwutocalmself <85773302+idkwutocalmself@users.noreply.github.com>
2021-09-07 21:04:02 -0700

    Add files via upload

---
+++ gamesofchance.js
@@ -0,0 +1,11 @@
+function game(game){
+	if (game == "home"){
+		window.location = "./gamesofchance.html"
+	}
+	if (game == "endlessEnemies"){
+		window.location = "./endlessEnemies.html"
+	}
+	if (game == "stoneAgeSurvival"){
+		window.location = "./stoneAgeSurvival.html"
+	}
+}
